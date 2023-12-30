898fd0fedda5795626128838bc280d33a14f2c61
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-12-30 21:03:51 +0100

    Setup basic app

---
+++ Demo/DemoApp/DemoApp/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using DemoApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		#region Constructor
+		private readonly IWeatherForecastService weatherForecastService;
+		private readonly ILogger<WeatherForecastController> logger;
+		public WeatherForecastController(IWeatherForecastService weatherForecastService, ILogger<WeatherForecastController> logger)
+		{
+			this.weatherForecastService = weatherForecastService;
+			this.logger = logger;
+		}
+		#endregion
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<WeatherForecast>>> Get()
+		{
+			var forecasts = await weatherForecastService.GetWeatherForecasts();
+			return Ok(forecasts);
+		}
+	}
+}
